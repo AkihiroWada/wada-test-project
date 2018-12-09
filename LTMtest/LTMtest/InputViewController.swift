10bd27b216153c85a2153f1c4c20f28067a2d97e
和田 明大 <wada_akihiro@abema.tv>
2018-12-09 20:08:36 +0900

    feat: タイトルとメッセージを入力画面に引き継ぐ

---
+++ LTMtest/LTMtest/InputViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InputViewController.swift
+//  LTMtest
+//
+//  Created by 和田 明大 on 2018/12/09.
+//  Copyright © 2018 和田 明大. All rights reserved.
+//
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet weak var titleField:UITextField!
+    @IBOutlet weak var messageField:UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
