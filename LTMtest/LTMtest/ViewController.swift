9a68d18834ac11946b93e265b142e834839328dd
和田 明大 <wada_akihiro@abema.tv>
2018-12-09 15:54:01 +0900

    feat: 初期プロジェクトファイルを作成

---
+++ LTMtest/LTMtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LTMtest
+//
+//  Created by 和田 明大 on 2018/12/09.
+//  Copyright © 2018 和田 明大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
