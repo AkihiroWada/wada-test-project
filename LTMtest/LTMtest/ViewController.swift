15ae3ca584f846a2f073efa46f21c27820697070
和田 明大 <wada_akihiro@abema.tv>
2018-12-09 16:36:24 +0900

    feat: マップを表示

--- LTMtest/LTMtest/ViewController.swift
+++ LTMtest/LTMtest/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let coordinate = CLLocationCoordinate2DMake(37.331652997806785, -122.03072304117417)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated:true)
     }
 
 
