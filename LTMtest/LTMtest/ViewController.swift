309f1a1a030860dc7a20d416a4b5627c95485340
和田 明大 <wada_akihiro@abema.tv>
2018-12-16 14:41:30 +0900

    feat: メッセージを値引き継ぎして表示する

--- LTMtest/LTMtest/ViewController.swift
+++ LTMtest/LTMtest/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    var titleValue:String = "タイトル"
+    var messageValiue:String = "メッセージ"
+    
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +24,46 @@ class ViewController: UIViewController {
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2DMake(37.331652997806785, -122.03072304117417)
-        annotation.title = "title"
-        annotation.subtitle = "subtitle"
+        annotation.title = titleValue
+        annotation.subtitle = messageValiue
         self.mapView.addAnnotation(annotation)
     }
     
-
-
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        print("クリックされました")
+        if control == view.rightCalloutAccessoryView {
+            let next = storyboard!.instantiateViewController(withIdentifier: "inputView") as?InputViewController
+            let _ = next?.view // ** hack code **
+            next?.titleField.text = titleValue
+            next?.messageField.text = messageValiue
+            self.present(next!,animated: true, completion: nil)
+        }
+    }
+    
+    // Called when the annotation was added
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.animatesDrop = true
+            pinView?.canShowCallout = true
+            
+            let rightButton: AnyObject! = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            pinView?.rightCalloutAccessoryView = rightButton as? UIView
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    @IBAction func goBack(_ segue:UIStoryboardSegue) {
+    }
 }
 
